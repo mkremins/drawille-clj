507deec5716020d0b65fc3a4807d9c1a146291e5
Max Kreminski <maxkreminski@gmail.com>
2014-05-22 11:43:05 -0400

    Use cljx

--- project.clj
+++ project.clj
@@ -4,4 +4,11 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[com.keminglabs/cljx "0.3.2"]]
+  :cljx {:builds [{:source-paths ["src"]
+                   :output-path "target/classes"
+                   :rules :clj}
+                  {:source-paths ["src"]
+                   :output-path "target/classes"
+                   :rules :cljs}]})
