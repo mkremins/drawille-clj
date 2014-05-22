1fd0f2001d6ee7372ad0dbed74dde3c733479fc2
Max Kreminski <maxkreminski@gmail.com>
2014-05-22 11:38:12 -0400

    Add license

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject mkremins/drawille-clj "0.1.0-SNAPSHOT"
   :description "Drawing in terminal with unicode braille characters"
   :url "https://github.com/mkremins/drawille-clj"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.6.0"]])
